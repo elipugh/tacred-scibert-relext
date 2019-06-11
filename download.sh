3f3b77bd1a126607f3461dbb17315b4d3dd941c7
Eli Pugh <epugh@stanford.edu>
2019-06-11 00:38:35 -0700

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-cd dataset; mkdir bert
-cd bert
+cd dataset;
 
-echo "==> Downloading glove vectors..."
+echo "==> Downloading BERT..."
 wget https://storage.googleapis.com/bert_models/2018_10_18/cased_L-12_H-768_A-12.zip
 
-echo "==> Unzipping glove vectors..."
+echo "==> Unzipping BERT..."
 unzip cased_L-12_H-768_A-12.zip
 rm cased_L-12_H-768_A-12.zip
+
+
+echo "==> Downloading SciBERT..."
+wget https://s3-us-west-2.amazonaws.com/ai2-s2-research/scibert/tensorflow_models/scibert_scivocab_cased.tar.gz
+
+echo "==> Unzipping SciBERT..."
+tar -xvzf scibert_scivocab_cased.tar.gz
+rm scibert_scivocab_cased.tar.gz
 
 echo "==> Done."
 
