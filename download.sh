ee6429d7c77cce1024452479cc49d2fce120b50f
Eli Pugh <epugh@stanford.edu>
2019-06-11 00:06:12 -0700

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-cd dataset; mkdir glove
-cd glove
+cd dataset; mkdir bert
+cd bert
 
 echo "==> Downloading glove vectors..."
-wget http://nlp.stanford.edu/data/glove.840B.300d.zip
+wget https://storage.googleapis.com/bert_models/2018_10_18/cased_L-12_H-768_A-12.zip
 
 echo "==> Unzipping glove vectors..."
-unzip glove.840B.300d.zip
-rm glove.840B.300d.zip
+unzip cased_L-12_H-768_A-12.zip
+rm cased_L-12_H-768_A-12.zip
 
 echo "==> Done."
+
 
